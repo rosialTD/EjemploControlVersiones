4a2078baebc5a94c34706c36bc7444fa5c0ef7cf
rosialTD <rosial@alumni.uv.es>
2025-02-03 20:22:47 +0100

    Perímetro rectángulo y cambio PI

--- Perim.R
+++ Perim.R
@@ -3,5 +3,8 @@ PerimetroCuadrado<-function(L){
   return(4*L)
 }
 LongitudCircunferencia<-function(R){
-  return(2*pi*R)
+  return(2*3.14159*R)
+}
+PerimetroRectangulo<-function(altura, base){
+  return(2*altura + 2*base)
 }
